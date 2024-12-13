4627ee857bcd90eea723cc74bf632d1261e94a92
Yuri Reyes <yreyesja@gmail.com>
2024-12-12 20:32:41 -0500

    feat[Villa]: Se implementa el EndPoint Post

--- MagicVilla_API/Controllers/VillaController.cs
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -3,6 +3,7 @@ using MagicVilla_API.Modelos;
 using MagicVilla_API.Modelos.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace MagicVilla_API.Controllers
 {
@@ -17,7 +18,7 @@ namespace MagicVilla_API.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("id:int")]
+        [HttpGet("id:int", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -36,6 +37,40 @@ namespace MagicVilla_API.Controllers
             }
 
             return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CrearVilla([FromBody] VillaDto villaDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // La siguiente condicion es para saber si encontro al menos un registro que coincide con el nombre que estoy intentando ingresar
+            if (VillaStore.villaList.FirstOrDefault(v => v.Nombre.ToLower() == villaDto.Nombre.ToLower()) != null)
+            {
+                ModelState.AddModelError("NombreExiste", "La Villa con ese Nombre ya existe");
+                    return BadRequest(ModelState);
+            }
+
+            if (villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+
+            if (villaDto.Id>0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+                
+            }
+            villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDto);
+
+            return CreatedAtRoute("GetVilla", new { id= villaDto.Id}, villaDto);
         }
 
     }
--- MagicVilla_API/Modelos/Dto/VillaDto.cs
+++ MagicVilla_API/Modelos/Dto/VillaDto.cs
@@ -1,8 +1,14 @@
-﻿namespace MagicVilla_API.Modelos.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Modelos.Dto
 {
     public class VillaDto
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+
+        
+        [Required] // para añadir una clase con ctrl + .
+        [MaxLength(30)]
+        public string Nombre { get; set; } // para añadir una variable con prop tab tab
     }
 }
